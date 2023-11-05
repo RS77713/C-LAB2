73cc6b49622a7cd5cb8d2b97f6eee975c0b811a6
Edgars Mihailovskis <61424717+RS77713@users.noreply.github.com>
2023-11-05 15:57:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>
-#include <cstdlib>
+
 
 using namespace std;
 
 
 
 int main() {
-	/*int taskVarCount = 20;
+	int taskVarCount = 20;
 	int studentCode = 83994;
 
 	int taskNr = studentCode % taskVarCount;
-	cout << studentCode % taskVarCount << endl;
-	cin >> taskNr;*/
+	cout << "Task number is: " << taskNr << endl;
 //taskNr 14
 
 
@@ -22,7 +21,7 @@ int main() {
 	double z1;
 	double z2;
 
-	cout << "Enter a value for the calculation" << endl;
+	cout << "Enter alfa value for the calculation" << endl;
 	cin >> userInput;
 
 	double senctantOfInput = 1.0/cos(2.0*userInput);
@@ -37,6 +36,3 @@ int main() {
 	return 0;
 
 }
-//(!) NOTE: C/C++ trigonometric functions take as parameter value in radians and not in degrees! 
-//User can enter initial data in radians, 
-// but if he asked to make input in degrees then program has to convert degrees to radians.
